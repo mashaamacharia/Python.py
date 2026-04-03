353de3905c7832e0af512a21c261a1c0216aa7ac
TechWorks <vm802208@gmail.com>
2026-04-03 19:53:13 +0300

    First class in python

---
+++ Class.py
@@ -0,0 +1,7 @@
+class BirthdayBoy:
+    def __init__(self,name,age):
+        self.name=Mashaa
+        self.age=fine
+
+    def vickie(self):
+        print(f"Happy birthday {self.name}! You are turning {self.age}!")

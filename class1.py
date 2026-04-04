205398688de9c5a77cbfe4708f64dac318b9b536
TechWorks <vm802208@gmail.com>
2026-04-04 20:59:40 +0300

    New class1

---
+++ class1.py
@@ -0,0 +1,9 @@
+class BirthDay:
+    def __init__(self,name,age):
+        self.name = Mashaa
+        self.age = fine
+
+    def vickie(self):
+        print(f"Hi {self.name} ! You are now {self.age} years old ")
+
+

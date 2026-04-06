17cda9d9fed91b178438a51bf8f1065895fda3dd
TechWorks <vm802208@gmail.com>
2026-04-06 13:59:11 +0300

    Dog class inside a class

--- class2.py
+++ class2.py
@@ -4,6 +4,16 @@ class Dog:
     
     def bark(self):
         print(f"{self.name} says woof woof woof")
+    
+    class dog1:
+        def __init__(self,name):
+            self.name=name
+        
+        def lick(self):
+            print(f"{self.name} says elf elf elf")
+
+    dog0=dog1("Tom")
+    dog0.lick()
 dog1=Dog("Bosko")
 dog1.bark()
     

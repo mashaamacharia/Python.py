28f8c2d464e5955f8e69b26186b9455fe98cd5db
TechWorks <vm802208@gmail.com>
2026-04-06 13:37:36 +0300

    Extra class

--- Class.py
+++ Class.py
@@ -6,7 +6,16 @@ class BirthdayBoy:
     def vickie(self):
         print(f"Happy birthday {self.name}! You are turning {self.age}!")
 
+class Wished:
+    def __init__(self,name):
+        self.name=name
+    
+    def siz(self):
+        print(f"Happy Birthday Big bro wishes from {self.name}")
+
 person=BirthdayBoy("Mashaa","24")
 person.vickie()
+person1=Wished("siz")
+person1.siz()
 
 

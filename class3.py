d0f8019a445b8333fa7ccdd13d720a540b97c243
TechWorks <vm802208@gmail.com>
2026-05-08 23:28:43 +0300

    Added another class in cass3.py

--- class3.py
+++ class3.py
@@ -4,6 +4,11 @@ class Dog:
 
     def bark(self):
         print(f"{self.name} says woof woof")
-        
+class Cat:
+    def__init__(self,name):
+        self.name=name
+
+cat1=Cat("Whiskers")
+print(cat1.name)
 dog1 = Dog("Buddy")
 print(dog1.name) 
